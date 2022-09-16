51781c8253e78d23785001770d187056918533dc
luiyi_V <luiyifernando5@gmail.com>
2022-09-16 10:05:51 -0500

    task 14 fixed

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -12,8 +12,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
-	if (!tree->left && !tree->right)
-		return (0);
 
 	h_left = binary_tree_height(tree->left);
 	h_right = binary_tree_height(tree->right);
